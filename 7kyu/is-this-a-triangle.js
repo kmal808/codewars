75cdd4f0342d9975c57c589e130f0430e60318e8
kmal808 <kjmalley7@gmail.com>
2022-03-27 23:27:51 -1000

    conditional with the && operator

---
+++ 7kyu/is-this-a-triangle.js
@@ -0,0 +1,26 @@
+//! IS THIS A TRIANGLE? 
+
+//* Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+//* (In this case, all triangles must have surface greater than 0 to be accepted).
+
+//? MY SOLUTION
+
+function isTriangle(a,b,c) {
+if (a + b > c && a + c > b && b + c > a) {
+  return true
+}
+  else return false
+}
+
+//? TOP SOLUTION
+
+function isTriangle(a,b,c)
+{
+   return a + b > c && a + c > b && c + b > a;
+}
+
+//? ONE LINER
+
+const isTriangle = (a$,b$,c$) => a$ + b$ > c$ && a$ + c$ > b$ && b$ + c$ > a$;
+
