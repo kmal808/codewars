6df352850d12f3fdb4bbc2a5648d3660865fbaf5
kmal808 <kjmalley7@gmail.com>
2022-04-01 22:17:59 -1000

    easy peasy

---
+++ 8kyu/function-3-multiplying-two-numbers.js
@@ -0,0 +1,15 @@
+// FUNCTION 3-MULTIPLYING TWO NUMBERS
+
+// Implement a function which multiplies two numbers.
+
+// my solution
+function multiply(a, b) {
+  return a * b 
+}
+
+// top solution 
+let multiply = (a, b) => a * b
+
+// some fancy shit with spread operator and the whole sha-bang
+const multiply = (...numbers) => numbers.reduce((acc, curr) => acc * curr);
+
