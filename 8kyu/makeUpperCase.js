dec9e8b55237d64cea748e159781cc9ddf692f52
kmal808 <kjmalley7@gmail.com>
2022-04-03 21:19:26 -1000

    to uppercase

---
+++ 8kyu/makeUpperCase.js
@@ -0,0 +1,10 @@
+// makeUpperCase
+
+// Write a function which converts the input string to uppercase.
+
+// my solution 
+function makeUpperCase(str) {return str.toUpperCase()}
+
+// top solution
+const makeUpperCase = str => str.toUpperCase();
+
