5df8a86d8cc80dda66b991ff9647e3a5a55bcfef
kmal808 <kjmalley7@gmail.com>
2022-04-04 21:39:46 -1000

    if else, forEach and ternary

---
+++ 8kyu/count-of-positives-sum-of-negatives.js
@@ -0,0 +1,48 @@
+// COUNT OF POSITIVES/SUM OF NEGATIVES
+
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+
+
+// my solution 
+function countPositivesSumNegatives(input) {
+  let positiveNums = 0
+  let negativeNums = 0
+  if (input === null || input.length === 0) { 
+    return []
+  } else { 
+    input.forEach((num) => num > 0 ? positiveNums++ : negativeNums += num)
+  }
+  return [positiveNums , negativeNums]
+}
+
+// top solution 
+function countPositivesSumNegatives(input) {
+  if (input == null || input.length == 0)
+    return [];
+  
+  var positive = 0;
+  var negative = 0;
+  
+  for (var i=0, l=input.length; i<l; ++i)
+  {
+    if (input[i] > 0)
+      ++ positive;
+    else
+      negative += input[i];
+  }
+  
+  return [positive, negative];
+}
+
+// fancy 
+function countPositivesSumNegatives(input) {
+  return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+}
