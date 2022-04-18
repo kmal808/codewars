0b5aac43a5b44ef4e7135dd901bb688267e7667f
kmal808 <kjmalley7@gmail.com>
2022-04-17 22:13:36 -1000

    map, toString, slice, join, toUpperCase

---
+++ 5kyu/rgb-to-hex-conversion.js
@@ -0,0 +1,36 @@
+// ! RGB TO HEX CONVERSION
+
+// The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+// The following are examples of expected output values:
+
+// rgb(255, 255, 255) // returns FFFFFF
+// rgb(255, 255, 300) // returns FFFFFF
+// rgb(0,0,0) // returns 000000
+// rgb(148, 0, 211) // returns 9400D3
+
+// * my solution
+
+function rgb(r, g, b){
+  return [r,g,b].map(function(x) {
+    return ('0'+Math.max(0, Math.min(255, x)).toString(16)).slice(-2);
+  }).join('').toUpperCase();
+}
+
+// * top solution
+
+function rgb(r, g, b){
+  return toHex(r)+toHex(g)+toHex(b);
+}
+
+function toHex(d) {
+    if(d < 0 ) {return "00";}
+    if(d > 255 ) {return "FF";}
+    return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+} 
+
+// * one liner 
+
+let rgb = (r, g, b) => [r,g,b].map( (item) => item>=255 ? 'FF' : item<=0 ? '00' : item.toString(16).toUpperCase()).join('')
