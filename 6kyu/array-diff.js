e88b200a7ef790afab4b20b90438934f3837bbea
kmal808 <kjmalley7@gmail.com>
2022-04-18 21:16:00 -1000

    for loop with a nested for loop and filter

---
+++ 6kyu/array-diff.js
@@ -0,0 +1,30 @@
+//! ARRAY.DIFF
+
+//* Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+//* It should remove all values from list a, which are present in list b keeping their order.
+
+//* arrayDiff([1,2],[1]) == [2]
+//* If a value is present in b, all of its occurrences must be removed from the other:
+
+//* arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+//? my solution 
+function arrayDiff(a, b) {
+  for (let c = 0; c < a.length; c++) {
+    for (let i = 0; i < b.length; i++) {
+      if (a[c] == b[i]) {
+        delete a[c]
+      }
+    }
+  }
+  return a.filter(Boolean)
+}
+
+//? top solution 
+function array_diff(a, b) {
+  return a.filter(e => !b.includes(e));
+}
+
+//? one liner
+var array_diff = (a, b) => a.filter(item => b.indexOf(item) < 0)
