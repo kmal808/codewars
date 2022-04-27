9d0f35e8f658b58ddb2c0c20651b5acd8ecd7300
kmal808 <kjmalley7@gmail.com>
2022-04-26 21:55:19 -1000

    simple, conditional

---
+++ 8kyu/grasshopper-personalized-message.js
@@ -0,0 +1,31 @@
+//! GRASSHOPPER-PERSONALIZED MESSAGE
+
+//* Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+//* Use conditionals to return the proper message:
+
+//* case	return
+//* name equals owner	'Hello boss'
+//* otherwise	'Hello guest'
+
+
+//? my solution 
+
+function greet (name, owner) {
+  if (name === owner) {
+    return 'Hello boss'
+    }
+    else
+    return 'Hello guest' 
+}
+
+//? top solution 
+
+function greet (name, owner) {
+  return name === owner ? 'Hello boss' :  'Hello guest';
+}
+
+//? one liner
+
+const greet = (name, owner) => name == owner? 'Hello boss': 'Hello guest';
+
