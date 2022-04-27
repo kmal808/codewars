26f186773a50601c73838f010e82fe180b40d0ad
kmal808 <kjmalley7@gmail.com>
2022-04-26 22:29:10 -1000

    big a18794 conditional

---
+++ 8kyu/rock-paper-scissors.js
@@ -0,0 +1,42 @@
+//! ROCK PAPER SCISSORS
+
+//* Rock Paper Scissors
+//* Let's play! You have to return which player won! In case of a draw return Draw!.
+
+//* Examples:
+
+//* rps('scissors','paper') // Player 1 won!
+//* rps('scissors','rock') // Player 2 won!
+//* rps('paper','paper') // Draw!
+
+
+//? my solution 
+
+const rps = (p1, p2) => {
+  if (p1 === 'rock' && p2 === 'scissors') return "Player 1 won!" 
+  if (p1 === 'scissors' && p2 === 'paper') return "Player 1 won!"
+  if (p1 === "paper" && p2 === 'rock') return "Player 1 won!"
+  if (p2 === 'rock' && p1 === 'scissors') return "Player 2 won!"
+  if (p2 === 'scissors' && p1 === 'paper') return "Player 2 won!"
+  if (p2 === 'paper' && p1 === 'rock') return "Player 2 won!"
+  if (p1 === 'rock' && p2 === 'rock') return "Draw!"
+  if (p1 === 'paper' && p2 === 'paper') return "Draw!"
+  if (p1 === 'scissors' && p2 === 'scissors') return "Draw!"
+};
+
+//? top solution 
+
+const rps = (p1, p2) => {
+  if (p1 === p2) return "Draw!";
+  var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+  if (p2 === rules[p1]) {
+    return "Player 1 won!";
+  }
+  else {
+    return "Player 2 won!";
+  }
+};
+
+//? one liner
+
+rps=(a,b)=>a==b?'Draw!':`Player ${/rp|ps|sr/.test(a[0]+b[0])?2:1} won!`;
