d90bd577c6ab80b458ee186eafa941d828c32e92
kmal808 <kjmalley7@gmail.com>
2022-04-29 22:23:01 -1000

    funny

---
+++ 8kyu/basic-variable-assignment.js
@@ -0,0 +1,21 @@
+//! BASIC VARIABLE ASSIGNMENT
+
+//* This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+
+//? code provided 
+
+var a == "code";
+var b == "wa.rs";
+var name == a + b;
+
+//? my solution
+
+var a = "code"
+var b = "wa.rs"
+var name = `${a}${b}`
+
+//? one liner and the best/most clever 
+
+let name = 'codewa.rs';
+
