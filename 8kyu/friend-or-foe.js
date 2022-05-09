ccb638594bf19bfbd55dfd6c083a3f3883b21493
kmal808 <kjmalley7@gmail.com>
2022-05-08 22:04:48 -1000

    filter it based on string length

---
+++ 8kyu/friend-or-foe.js
@@ -0,0 +1,33 @@
+//! FRIEND OR FOE? 
+
+//* Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+//* If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+//* Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+//* i.e.
+
+//* friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+//* Note: keep the original order of the names in the output.
+
+
+
+//? MY SOLUTION
+
+function friend(friends){
+  const words = friends.filter(x => isNaN(x))
+  return words.filter(friend => friend.length == 4)
+}
+
+//? TOP SOLUTION
+
+function friend(friends){
+  return friends.filter(n => n.length === 4)
+}
+
+//? ONE LINER
+
+const friend = friends => friends.filter(friend => friend.length == 4);
+
+
