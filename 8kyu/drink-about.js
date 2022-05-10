00929a4cef4879c8e879c3113e46b9948413e089
kmal808 <kjmalley7@gmail.com>
2022-05-09 22:25:59 -1000

    big arse conditional

---
+++ 8kyu/drink-about.js
@@ -0,0 +1,43 @@
+//! DRINK ABOUT 
+
+//* Kids drink toddy.
+//* Teens drink coke.
+//* Young adults drink beer.
+//* Adults drink whisky.
+//* Make a function that receive age, and return what they drink.
+
+//* Rules:
+
+//* Children under 14 old.
+//* Teens under 18 old.
+//* Young under 21 old.
+//* Adults have 21 or more.
+//* Examples: (Input --> Output)
+
+//* 13 --> "drink toddy"
+//* 17 --> "drink coke"
+//* 18 --> "drink beer"
+//* 20 --> "drink beer"
+//* 30 --> "drink whisky"
+
+
+
+//? MY SOLUTION
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) return 'drink toddy'
+  if (old < 18 && old >= 14) return 'drink coke'
+  if (old < 21 && old >= 18) return 'drink beer'
+  if (old >= 21) return 'drink whisky'
+}
+
+//? TOP SOLUTION 
+
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
+
+//? ONE LINER
+
+const peopleWithAgeDrink = old => 'drink ' + (old < 14 ? 'toddy' : old < 18 ? 'coke' : old < 21 ? 'beer' : 'whisky')
