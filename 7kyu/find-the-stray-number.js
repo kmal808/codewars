89b6879c4dc9c0ad4dbeb7839644a99459e0f8d7
kmal808 <kjmalley7@gmail.com>
2022-05-10 22:14:53 -1000

    sort it, conditional checking the numbers

--- 7kyu/find-the-stray-number.js
+++ 7kyu/find-the-stray-number.js
@@ -11,3 +11,26 @@
 //* [17, 17, 3, 17, 17, 17, 17] ==> 3
 
 
+
+//? MY SOLUTION
+
+function stray(numbers) {
+  const arr = numbers.sort((a,b) => a -b) 
+  if (arr[0] == arr[1]) {
+    return arr[arr.length-1]
+  } else {
+    return arr[0]
+  }
+}
+
+//? TOP SOLUTION 
+
+function stray(numbers){
+  for (var i in numbers){
+     if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])){return numbers[i]}
+  }
+}
+
+//? ONE LINER
+
+const stray = nums => nums.reduce((a, b) => a ^ b);
