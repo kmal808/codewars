ac8d45ab0852991801e2237d706a299d81cf12ba
kmal808 <kjmalley7@gmail.com>
2022-05-11 22:18:21 -1000

    simple maff

--- 8kyu/volume-of-a-cuboid.js
+++ 8kyu/volume-of-a-cuboid.js
@@ -1,3 +1,25 @@
 //! VOLUME OF A CUBOID 
 
 //* Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+
+
+//? MY SOLUTION 
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+//? TOP SOLUTION 
+
+class Kata {
+  static getVolumeOfCuboid(l, w, h) {
+    return l * w * h;
+  }
+};
+
+//? ONE LINER
+
+let Kata = {getVolumeOfCuboid : (a, b, c) => a * b * c};
