e7623c38707b614c34cf2fddee7fe0863aeba816
kmal808 <kjmalley7@gmail.com>
2022-08-24 23:33:01 -1000

    pass arg2 to arg1.endsWith

---
+++ 7kyu/string-ends-with.js
@@ -0,0 +1,38 @@
+//! STRING ENDS WITH
+
+//* Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+//* Examples:
+
+//* solution('abc', 'bc') // returns true
+//* solution('abc', 'd') // returns false
+
+//? PREP
+//? P = takes in two args, both strings.
+//? R = return true if first arg ends with the second arg
+//? E = empty string, second string has a greater length than the first
+//? P = compare arg1 against arg2 starting at a postion of arg2.length
+
+//* MY SOLUTION
+const solution = (str, ending) => str.endsWith(ending)
+
+//* TOP SOLUTION
+function solution(str, ending) {
+	return str.endsWith(ending)
+}
+
+//* CLEVER AF
+function solution(str, ending) {
+	return str.slice(0 - ending.length) === ending
+}
+
+//* WTF LOL
+function solution(str, ending) {
+	return (
+		str
+			.split('')
+			.reverse()
+			.join('')
+			.search(ending.split('').reverse().join('')) === 0
+	)
+}
